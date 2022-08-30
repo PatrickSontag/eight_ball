58524fd4c66ffec00fdef8c40531473737468461
Patrick Sontag <pmsontag84@gmail.com>
2022-08-30 16:57:38 -0500

    ball and message

---
+++ src/EightBall.js
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+import ANSWERS from "./answers";
+
+const EightBall = () => {
+    return(
+        <div>
+            <div class="circle">
+                <div class="ball-text">Think of a Question</div>
+            </div>
+        </div>
+    )
+}
+
+export default EightBall;
