c835045ade8c36954f338f15c4eba4946300af7a
Patrick Sontag <pmsontag84@gmail.com>
2022-09-27 18:59:41 -0500

    establish state

--- src/EightBall.js
+++ src/EightBall.js
@@ -2,10 +2,12 @@ import React, { useState } from "react";
 import ANSWERS from "./answers";
 
 const EightBall = () => {
+    const [color, setColor] = useState("black");
+    const [message, setMessage] = useState("Think of a Question");
     return(
         <div className="app-container">
-            <div className="eightball">
-                <div className="ball-text">Think of a Question</div>
+            <div className={`eightball ${color}`}>
+                <div className="ball-text">{ message }</div>
             </div>
         </div>
     )
