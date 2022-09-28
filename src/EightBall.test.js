f7b911b74fbbe72e89f8a5e61be8642a0fc61945
Patrick Sontag <pmsontag84@gmail.com>
2022-09-28 10:22:15 -0500

    component snapshot test

--- src/EightBall.test.js
+++ src/EightBall.test.js
@@ -5,3 +5,8 @@ import EightBall from './EightBall';
 test('it rendersnwiyhout crashing', () => {
   render(<EightBall />);
 });
+
+test('it matches snapshot', () => {
+    const {asFragment} = render(<EightBall />);
+    expect(asFragment()).toMatchSnapshot()
+})
