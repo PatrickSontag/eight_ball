1bcfacf9decfb53e520ac7b22ac498948bab2964
Patrick Sontag <pmsontag84@gmail.com>
2022-09-27 19:26:52 -0500

    change state button

--- src/EightBall.js
+++ src/EightBall.js
@@ -4,11 +4,22 @@ import ANSWERS from "./answers";
 const EightBall = () => {
     const [color, setColor] = useState("black");
     const [message, setMessage] = useState("Think of a Question");
+
+    const numberOfResponses = ANSWERS.length;
+    function handleClick() {
+        let randomNum = Math.floor(Math.random() * numberOfResponses) + 1;
+        let response = ANSWERS[randomNum];
+        setColor(response.color);
+        setMessage(response.msg);
+        console.log("response:", response, "response.color", response.color, "response.msg", response.msg);
+    }
     return(
         <div className="app-container">
-            <div className={`eightball ${color}`}>
-                <div className="ball-text">{ message }</div>
-            </div>
+            <button className="btn" onClick={handleClick}>
+                <div className={`eightball ${color}`}>
+                    <div className="ball-text">{ message }</div>
+                </div>
+            </button>
         </div>
     )
 }
